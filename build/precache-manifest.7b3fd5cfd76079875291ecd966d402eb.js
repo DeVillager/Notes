95893422d97cdd86dacae89b32622f12d6188b8c
Kirage <tommi.makelainen@tuni.fi>
2020-07-01 02:33:44 +0300

    frontend build added. single page app implemented

---
+++ build/precache-manifest.7b3fd5cfd76079875291ecd966d402eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39784b499fe1eb4215eb1dd3d01959bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1da766bebac9d46c17d1",
+    "url": "/static/js/2.168be5f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.168be5f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9b89f909b8454aca592",
+    "url": "/static/js/main.5ea3bd35.chunk.js"
+  },
+  {
+    "revision": "3094d3d52de3e8894b8c",
+    "url": "/static/js/runtime-main.51f2d2e6.js"
+  }
+]);
